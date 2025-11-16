cb2a9534aaac70ceeac08e0b2367f5076b630141
rajank18 <rajankanzariya1806@gmail.com>
2025-11-16 14:08:26 +0530

    minor fixes

--- frontend/src/routes/AuthRoutes.jsx
+++ frontend/src/routes/AuthRoutes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
 // import Auth from '../layout/Auth.jsx'
 // import Login from '../pages/auth/Login.jsx'
-// import Login from '../pages/auth/Login.jsx'
+import Login from '../pages/auth/Login.jsx'
 import ForgotPassword from '../pages/auth/ForgotPassword.jsx'
 import ResetPassword from '../pages/auth/ResetPassword.jsx'
 import ChangePassword from '../pages/auth/ChangePassword.jsx'
